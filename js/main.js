e68a09d084b1934c6abbb0c06a3fb8249744aedf
kirillstepanchuk <kirill.stepanchuk.96@mail.ru>
2021-01-23 18:33:00 +0300

    something

--- js/main.js
+++ js/main.js
@@ -1,26 +1,47 @@
 "use strict";
 
+const HALL = {
+    PLACE_VIP: {
+        cost: 1000,
+        className: "place--vip",
+    },
+    PLACE_HIGH_COST: {
+        cost: 750,
+        className: "place--high-price",
+    },
+    PLACE_MEDIUM_COST: {
+        cost: 500,
+        className: "place--medium-price",
+    },
+    PLACE_LOW_COST: {
+        cost: 250,
+        className: "place--low-price",
+    },
+}
+
 const bookedTicketCount = document.querySelector(".booked-ticket-count");
 const bookedTicketCost = document.querySelector(".booked-ticket-cost");
 const placeUnits = document.querySelectorAll(".place");
-const placeUnitsVip = document.querySelectorAll(".place--vip");
 
-const changeTicketCost = (evt) => {
-    if (!evt.target.classList.contains("place--booked")) {
-        bookedTicketCost.textContent += evt.target.dataset.cost; 
-        console.log('evt.target.cost: ', evt.target.dataset.cost);
-    } else {
-        bookedTicketCost.textContent -= evt.target.dataset.cost; 
+const addTicketCost = (evt) => {
+    if (evt.target.classList.contains("place--vip") && evt.target.classList.contains("place--booked")) {
+        bookedTicketCost.textContent = +bookedTicketCost.textContent + +HALL.PLACE_VIP.cost
+    } else if (evt.target.classList.contains("place--vip") && !evt.target.classList.contains("place--booked")) {
+        bookedTicketCost.textContent = +bookedTicketCost.textContent - +HALL.PLACE_VIP.cost
+    } else if (evt.target.classList.contains("place--high-price") && evt.target.classList.contains("place--booked")) {
+        bookedTicketCost.textContent = +bookedTicketCost.textContent + +HALL.PLACE_HIGH_COST.cost
+    } else if (evt.target.classList.contains("place--high-price") && !evt.target.classList.contains("place--booked")) {
+        bookedTicketCost.textContent = +bookedTicketCost.textContent - +HALL.PLACE_HIGH_COST.cost
+    } else if (evt.target.classList.contains("place--medium-price") && evt.target.classList.contains("place--booked")) {
+        bookedTicketCost.textContent = +bookedTicketCost.textContent + +HALL.PLACE_MEDIUM_COST.cost
+    } else if (evt.target.classList.contains("place--medium-price") && !evt.target.classList.contains("place--booked")) {
+        bookedTicketCost.textContent = +bookedTicketCost.textContent - +HALL.PLACE_MEDIUM_COST.cost
+    } else if (evt.target.classList.contains("place--low-price") && evt.target.classList.contains("place--booked")) {
+        bookedTicketCost.textContent = +bookedTicketCost.textContent + +HALL.PLACE_LOW_COST.cost
+    } else if (evt.target.classList.contains("place--low-price") && !evt.target.classList.contains("place--booked")) {
+        bookedTicketCost.textContent = +bookedTicketCost.textContent - +HALL.PLACE_LOW_COST.cost
     }
 }
-
-
-
-// placeUnitsVip.forEach((item) => {
-//     item = {
-//         cost: 1000,
-//     }
-// })
 
 const changePlaceConditionOnBooked = (evt) => {
     if (!evt.target.classList.contains("place--booked")) {
@@ -33,10 +54,10 @@ const changePlaceConditionOnBooked = (evt) => {
     }
 }
 
-for (let placeUnit of placeUnitsVip) {
-    placeUnit.addEventListener("click", changeTicketCost);
+for (let placeUnit of placeUnits) {
+    placeUnit.addEventListener("click", changePlaceConditionOnBooked);
 }
 
 for (let placeUnit of placeUnits) {
-    placeUnit.addEventListener("click", changePlaceConditionOnBooked);
+    placeUnit.addEventListener("click", addTicketCost);
 }
